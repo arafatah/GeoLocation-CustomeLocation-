d4a8c2ae6885b2e15c5cc9112d67a99cc1c26eb0
arafatah <arafathowlader890@gmail.com>
2024-11-11 17:15:13 +0600

    geolocation

--- script.js
+++ script.js
@@ -11,11 +11,15 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-navigator.geolocation.getCurrentPosition(
-  function () {
-    
-  },
-  function () {
-    alert('Could not get your position');
-  }
-);
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(longitude, latitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
